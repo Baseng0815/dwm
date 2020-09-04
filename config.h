3258eefb4cf755e403408ca5f0bc69bbb75276c4
Baseng0815 <1shedex2@gmail.com>
2020-09-05 01:03:21 +0200

    add swallow patch

--- config.h
+++ config.h
@@ -27,9 +27,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class            instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",           NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
+	{ "st-256color",    NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,             NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -79,7 +80,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 
-	{ MODKEY,                       XK_Return, spawn,          { .v = termcmd} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          { .v = termcmd} },
 	{ MODKEY,                       XK_w, 	   killclient,     {0} },
 
 	/*
